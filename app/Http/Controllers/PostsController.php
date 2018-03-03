cc6dcf3d001dbe1646f4edfda44e32bb475fd541
mtotodev05 <mtotodev05@gmail.com>
2018-03-03 23:39:01 +0300

    Updated Controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use App\Post;
 use Illuminate\Http\Request;
@@ -12,4 +13,14 @@ class PostsController extends Controller
 
         return view('posts.index',compact('posts'));
     }
+    public function favoritePost(Post $post){
+        Auth::user()->favorites()->attach($post->id);
+        return back();
+    }
+
+    public function unFavoritePost(Post $post){
+        Auth::user()->favorites()->detach($post->id);
+        return back();
+    }
+
 }
